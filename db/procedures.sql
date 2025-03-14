82903a09ca90c60ffe4de5a6805c49279629c761
matthew13 <matthew.coder@outlook.com>
2025-03-14 12:22:50 -0300

    add:Agenda 01 TI3 - Procedurers

--- db/procedures.sql
+++ db/procedures.sql
@@ -1,1 +1,16 @@
 --Agenda 01 T.I.3
+
+--1 - Procedure para atualizar o valor dos apartamentos
+DELIMITER //
+
+CREATE PROCEDURE AtualizarValorApartamento(
+    IN p_codigo_cond INT,  -- Identificador do condomínio
+    IN p_percentual INT    -- Percentual de aumento (10% = 10)
+)
+BEGIN
+    UPDATE apartamento
+    SET valor = valor + (valor * p_percentual / 100)
+    WHERE codigo_cond = p_codigo_cond;
+END //
+
+DELIMITER ;
