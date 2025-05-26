d91d4cf2263b951ec2841be2b73add9be4de5a1e
matthew13 <matthew.coder@outlook.com>
2025-05-26 02:26:50 -0300

    adicioando agenda 12

--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -141,4 +141,19 @@ class Usuario
             return FALSE;
         }
     }
+
+
+    public function listaCadastrados()
+    {
+        require_once 'ConexaoBD.php';
+        $con = new ConexaoBD();
+        $conn = $con->conectar();
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT idusuario, nome FROM usuario;";
+        $re = $conn->query($sql);
+        $conn->close();
+        return $re;
+    }
 }
--- src/db/projetoFinal.sql
+++ src/db/projetoFinal.sql
@@ -96,3 +96,20 @@ CREATE TABLE IF NOT EXISTS `projeto_final`.`outrasformacoes` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+
+
+
+-- Table `projeto_final`.`administrador`
+CREATE TABLE IF NOT EXISTS `projeto_final`.`administrador` (
+`idadministrador` INT(11) NOT NULL AUTO_INCREMENT,
+`nome` VARCHAR(45) NULL DEFAULT NULL,
+`cpf` VARCHAR(11) NOT NULL,
+`senha` VARCHAR(45) NOT NULL,
+PRIMARY KEY (`idadministrador`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
+
+INSERT INTO `projeto_final`.`administrador`
+(`nome`, `cpf`, `senha`) VALUES ('Bia',
+'22222222222', 'bia123');
